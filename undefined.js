502844a6d4b9f31edb1fbfb26a0c9fdb58f7a9b9
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-11-28 18:34:54 +0530

    3rd Day of Learning JavaScript

---
+++ undefined.js
@@ -0,0 +1,20 @@
+console.log(a); // undefined
+var a = 7;
+console.log(a);  
+console.log(x); // Not defined
+
+
+var x;
+if(x === undefined){
+    console.log("x is undefined");
+}else{
+    console.log("x is not undefined");
+}
+
+var b;
+console.log(b);
+b = 10;
+console.log(b);
+b = "Hello World";
+console.log(b);
+
