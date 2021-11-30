3cdd22c68163978db34dfac8788f4cb5a998df59
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-11-30 21:18:07 +0530

    4th Day Of Learning JavaScript

---
+++ let & const.js
@@ -0,0 +1,23 @@
+// console.log(b); // Undefined
+// console.log(a); // cannot access a before intialization
+// let a = 10;
+// var b = 100;
+
+
+//  let a = 12;  //  SyntaxError: redeclaration of let a
+// var a = 122; // SyntaxError: redeclaration of let a
+
+
+// let a;
+// a = 100;
+// console.log(a);
+
+// const b;
+// b = 12;
+// console.log(b);
+
+// const b = 12;
+// console.log(b);
+
+// b = 22;
+// console.log(b);
