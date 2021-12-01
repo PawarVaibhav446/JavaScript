4412df0f5cc2dbc5def3a2a1b3166a6b2ba9f52d
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-12-01 19:52:43 +0530

    5th day Of Learning JavaScript

---
+++ BlockScope.js
@@ -0,0 +1,31 @@
+// if (true) {
+//     console.log("Vaibhav Pawar");
+// }
+
+// var a = 100;
+
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+
+// console.log(a);
+
+// let x = 20;
+// {
+//     var x = 30;
+// }
+
+var x = 20;
+{
+    let x = 30;
+    console.log(x);
+}
+
+console.log(x);
