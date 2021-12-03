7565118a46c936338c2485ddf7b755bf97d54f00
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2021-12-03 20:25:22 +0530

    Studied Closures. Imp for interviews

--- JavaScript File/Closures.js
+++ JavaScript File/Closures.js
@@ -1,8 +1,43 @@
-function x() {
-    var a = 7;
-    function y() {
-        console.log(a);
+// function x() {
+//     var a = 7;
+//     function y() {
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+// function x() {
+//     var a = 7;
+//     function y() {
+//         console.log(a);
+//     }
+//     return y;
+// }
+// var z = x();
+// console.log(z);
+// z();
+
+
+// function x() {
+//     var a = 7;
+//     function y() {
+//         console.log(a);
+//     }
+//     a = 20;
+//     y();
+// }
+// x();
+
+function z() {
+    var b = 900;
+    function x() {
+        var a = 7;
+        function y() {
+            console.log(a, b);
+        }
+        y();
     }
-    y();
+    x();
 }
-x();
+z();
