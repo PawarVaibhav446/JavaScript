e889a6436289a593e4202d537dded79d2b8e8341
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2022-01-29 11:18:09 +0530

    Fat Arrow Functions

---
+++ ES6/ArrowFunctions.js
@@ -0,0 +1,20 @@
+/* 
+Arrow functions allows a short syntax for writing function expressions.
+You don't need the function keyword, the return keyword, and the curly brackets.
+*/
+
+// Declaration:
+
+// Eg. 1)
+let x = (x,y) => {
+    x * y
+};
+
+// 2)
+ hello = () => {
+    console.log("Hello World");
+};
+
+hello();
+
+// Arrow functions are not hoisted. They must be defined before they are used.
