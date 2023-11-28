103cc70227b4eea156ad0aea6627fb57c3302606
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2023-11-28 21:10:22 +0530

    Revised JS

---
+++ JS Refreshment/MapAndFilter.js
@@ -0,0 +1,10 @@
+
+let names = ["Orums", "ReS1", "Nrnz"]
+
+names.map((name) => {
+    console.log(name);
+})
+
+names.filter((name) => {
+    return name !== "Nrnz"
+})
---
+++ JS Refreshment/TernaryOperators.js
@@ -0,0 +1,6 @@
+
+let age = 21
+let name = 'Vaibhav'
+
+let ans = age > 20 ? 'Vaibhav is Old' : 'Vaibhav is Young'
+console.log(ans);
